f2b5c2db14fbba8d034df58d5fe6b3f3ea578a4b
sharjeel-siddiqui12 <sharjeelsiddiqui00@gmail.com>
2024-04-29 22:50:59 +0500

    latest comm

--- file.php
+++ file.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>PHP CODES</h1>
 
     <?php
   echo "Hello, World!";
 ?>
-
-</body>
-</html>
